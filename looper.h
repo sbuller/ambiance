231b1a4e4cf41125cba66c937b8401cd7cc13dc9
Stefan Buller <stefan.buller@gmail.com>
2012-06-14 15:39:02 -0500

    Some formatting issues

--- looper.h
+++ looper.h
@@ -27,14 +27,13 @@ class Looper : public QObject
 {
 	Q_OBJECT
 
-		MediaSource  m_src;
+	MediaSource  m_src;
 	MediaObject* m_obj;
 	AudioOutput* m_out;
 
-	public:
+public:
 	explicit Looper(QString path, QObject *parent = 0);
 
-	public slots:
-		void loop();
-
+public slots:
+	void loop();
 };
